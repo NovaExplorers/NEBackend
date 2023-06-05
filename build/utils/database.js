ae07a2ad1fbbbbe1c986f0afe8daee775987dfc4
Rime Kabbaj <rimekabbaj@iMac-de-Rime.local>
2023-06-05 03:16:20 +0100

    Revert "First backend commit 🚀"
    This reverts commit 0d25df9334516384ab122e15d3f968f4b11798af.

--- build/utils/database.js
+++
@@ -1,8 +0,0 @@
-import * as monk from 'monk';
-import { dbUrl } from '@config/db.config';
-let db;
-const initDb = () => {
-    db = monk.default(dbUrl);
-};
-const getDb = () => db;
-export { initDb, getDb };
--- src/models/User.model.ts
+++
@@ -1,9 +0,0 @@
-import * as yup from 'yup';
-
-export default yup.object().shape({
-    _id: yup.string(),
-    username: yup.string()
-                .required('You must specify a username'),
-    password: yup.string()
-                 .required('You must specify a password'),
-})
